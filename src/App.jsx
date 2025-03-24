91d298b03039941512b1db96d9e3b316f652f08f
Erick Chavez <erickchavezescobar@gmail.com>
2025-03-23 20:04:07 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
+      <h1>The Guardian of the Bat Hill</h1>
+      <p>This is the beginning of our interactive story.</p>
+    </div>
+  )
+}
+
+export default App
